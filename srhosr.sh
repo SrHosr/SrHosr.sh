40239e86ee88ecc92dd117d7bbd864482dddd4ef
SrHosr <112656776+SrHosr@users.noreply.github.com>
2022-09-01 16:07:47 -0300

    srhosr.sh

---
+++ srhosr.sh
@@ -0,0 +1,46 @@
+termux-setup-storage
+apt update -y
+apt upgrade -y
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install fish -y
+pkg install ruby -y
+pkg install git -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install hydra -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install wget -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install golang -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+pkg install toilet -y
+pkg install sl -y
+pkg install vim -y
+pkg install tch -y
+pkg install zsh -y
+pkg install fortune -y
+pkg install zsh -y
+gem install lolcat
+figlet -f big espere... | lolcat --seed=18
+git clone https://github.com/SrHoustX/simple-edit
+sed 's+THBD+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+THBD+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+figlet -f big pronto | lolcat --seed=18
